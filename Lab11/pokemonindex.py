445e899bb824214407c99f73e5e293704459c168
gloomtrop <axel.rooth16@gmail.com>
2020-01-18 14:22:54 +0100

    klar search funk

--- Lab11/pokemonindex.py
+++ Lab11/pokemonindex.py
@@ -1,4 +1,4 @@
-
+import random
 
 class Pokemon:
 
@@ -17,13 +17,13 @@ class Pokemon:
         self.legendary = legendary
 
     def attackDmg(self):
-        return self.attack
+        return self.attack + random.randint(0,1)*self.spAtk
     
-    def health(self):
-        return self.hp
+    def health(self, attack_by_enemy):
+        return self.hp - attack_by_enemy + random.randint(0,1)*self.defense
 
     def __str__(self):
-        return ("Din pokemon heter " + self.namn + " och har dessa attributer\n" +
+        return ("Din pokemon heter " + self.namn.title() + " och har dessa attributer\n" +
             "Typ1: " + self.typ1 + "\n" +
             "Typ2: " + self.typ2 + "\n" +
             "Total: " + self.total + "\n" +
@@ -40,22 +40,34 @@ class Pokemon:
 def readFiles():
 
     pokemonList = list()
-    fil = open("/Users/Axel Rooth/Desktop/KTH/Data/Tillda/Lab1/pokemon.txt", "r") 
+    fil = open("/Users/Axel Rooth/Desktop/KTH/Data/Tillda/Lab11/pokemon.txt", "r") 
     textFile = fil.readlines()
     fil.close()
     for line in textFile:
         pokL = line.split(",")
-        pokemonList.append(Pokemon(pokL[1], pokL[2], pokL[3], pokL[4], pokL[5], pokL[6], pokL[7], pokL[8], pokL[9], pokL[10], pokL[11], pokL[12]))    
+        pokemonList.append(Pokemon(pokL[1].lower(), pokL[2], pokL[3], pokL[4], pokL[5], pokL[6], pokL[7], pokL[8], pokL[9], pokL[10], pokL[11], pokL[12]))    
     
     return pokemonList
+
+def search(pokemonList, user_search):
+    pos = 0
+    found = False
+
+    while pos< len(pokemonList) and not found:
+        if pokemonList[pos].namn == user_search:
+            found = True
+            print((pokemonList[pos]))
+        else:
+            pos += 1
+
 
 def main():
     pokemonList = readFiles()
     del pokemonList[0]
-    print(str(pokemonList[0].namn) + " attackerar med " +str(pokemonList[0].attackDmg()) + " Attack" )
-    print(str(pokemonList[0].namn) + " har " +str(pokemonList[0].health()) + " HP" )
-    # print(pokemonList[0])
-    # herregud
+    
+    user_search = input("What pokemon do you want to search for? ").lower()
+    search(pokemonList, user_search)
+    
 
 main()
 
